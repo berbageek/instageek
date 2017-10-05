4a2fddeda3363dcd288d7d18c9877b5031e13c46
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2017-10-05 17:06:11 +0700

    User seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,13 +13,21 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(\App\Models\User::class, function (Faker $faker) {
+$factory->define(
+    \App\Models\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+
+        'username' => $faker->userName,
+        'bio'      => $faker->paragraph(),
+        'website'  => $faker->url,
+        'phone'    => $faker->phoneNumber,
+        'sex'      => $faker->randomElement(['M', 'F', 'O']),
     ];
-});
+}
+);
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserSeeder::class);
+        factory(\App\Models\User::class)->times(10)->create();
     }
 }
