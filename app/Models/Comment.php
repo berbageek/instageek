b439e97346f969c714e1299718a10d695d0994be
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2017-10-05 20:53:35 +0700

    Define relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
